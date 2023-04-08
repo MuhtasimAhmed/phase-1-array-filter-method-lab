eea64392c314041938fae1e8cc00758d353bdda6
Muhtasim <mts.ahmed.1010@gmail.com>
2023-04-07 22:20:40 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(array, string){
+    let matchedDrivers = array.filter(function(element){
+        return element.toUpperCase() === string.toUpperCase()
+
+    })
+    return matchedDrivers
+}
+function fuzzyMatch(array, string){
+    let matchedDrivers = array.filter(function(element){
+        return element[0] === string[0]
+    })
+    return matchedDrivers
+}
+function matchName(array, string){
+    let matchedDrivers = array.filter(function(element){
+        return element.name === string
+    })
+    return matchedDrivers
+}
